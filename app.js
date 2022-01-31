b36991625490961c9280808d074b422a8b5d74b4
Andre Athari <athariandre@gmail.com>
2022-01-31 10:12:40 -0600

    node class setup

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ function createMatrix(width,height){
     for(var i = 0; i < height; i++){
      matrix[i] = new Array(width);
     }
+    return matrix;
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+class Node{
+  constructor(x,y,)
+}
+function findPath(matrix){
+
+}
+
 
 
