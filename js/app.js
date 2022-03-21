e88a190be34408f927314689db7be0148854ec7f
Andre Athari <athariandre@gmail.com>
2022-03-21 14:30:51 -0500

    organization

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function createMatrix(width,height){
+    var matrix = [];
+    for(var i = 0; i < height; i++){
+     matrix[i] = new Array(width);
+    }
+    return matrix;
+}
+matrix = fillMatrix(createMatrix(4,4));
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
